fb9fad4270527a053c8d94735e45d9ba0ee5ecda
sipacate <sipacate@gmail.com>
2015-06-10 15:26:20 -0400

    Updated config.js for ckeditor so that i tags and other tags w ill not be deleted from the source code

--- admin/assets/ckeditor/config.js
+++ admin/assets/ckeditor/config.js
@@ -5,7 +5,11 @@ For licensing, see LICENSE.html or http://ckeditor.com/license
 
 CKEDITOR.editorConfig = function( config )
 {
+    config.allowedContent = true;
+    config.extraAllowedContent = 'p(*)[*]{*};div(*)[*]{*};li(*)[*]{*};ul(*)[*]{*}';
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
 };
+// ALLOW <i></i>
+CKEDITOR.dtd.$removeEmpty.i = 0;
